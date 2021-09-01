02543dd6176266aa1900a4baa59a800dfd176ce5
Aleksey <aleksey.ilichenko@yandex.ru>
2021-09-02 02:52:24 +0300

    graphql added to backend

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,8 +1,16 @@
+import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      context: ({ req }) => ({ req }),
+      playground: true,
+    }),
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
