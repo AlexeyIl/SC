51247941ac0c49543ce24485390c70401501109a
Aleksey <aleksey.ilichenko@yandex.ru>
2021-09-03 04:27:51 +0300

    typeorm added to nest

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { environment } from '../environments/environment';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      ...environment.connection,
+      entities: [],
+    }),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       context: ({ req }) => ({ req }),
