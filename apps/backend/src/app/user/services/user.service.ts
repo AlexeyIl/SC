72bb375e0cb2063b8a4c84675449931f3f4926b2
Aleksey <aleksey.ilichenko@yandex.ru>
2021-09-07 01:23:49 +0300

    user resolver added to backend

--- apps/backend/src/app/user/services/user.service.ts
+++ apps/backend/src/app/user/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { UserEntity } from '../entities/user.entities';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
 export class UserService {
