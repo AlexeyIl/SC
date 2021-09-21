e9f209f96979ef19167bc00f9186300807fae2b8
Aleksey <aleksey.ilichenko@yandex.ru>
2021-09-21 23:56:48 +0300

    index.ts of shared storage module fixed

--- libs/shared/utils/storage/src/index.ts
+++ libs/shared/utils/storage/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/storage.module';
+export * from './lib/services/base-cookie.service'
+export * from './lib/storage/base-cookie.storage'
+export * from './lib/storage/base-local.storage'
+export * from './lib/storage/base-session.storage'
